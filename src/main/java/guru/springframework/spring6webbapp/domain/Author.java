8e9e7636ebef6d8ee15c29ea586dfeca0c1ce783
Italo Honorato <italohonorato@gmail.com>
2025-02-06 17:15:22 -0300

    adding equals, hashCode and toString methods

--- src/main/java/guru/springframework/spring6webbapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webbapp/domain/Author.java
@@ -13,6 +13,7 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
+    private String lastName;
 
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
@@ -41,5 +42,28 @@ public class Author {
         this.lastName = lastName;
     }
 
-    private String lastName;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
